e316e94f9ac2b496c062bc548f9c46e8cd77bc81
tp.gigas <tp.gigas@gmail.com>
2021-06-16 15:25:56 +0900

    Update app.js: kyepress event 추가.

--- webSocket/app/app.js
+++ webSocket/app/app.js
@@ -1,7 +1,6 @@
 const socket = io('ws://localhost:8080');
 
 socket.on('message', text => {
-
     const el = document.createElement('li');
     el.innerHTML = text;
     document.querySelector('ul').appendChild(el)
@@ -9,10 +8,13 @@ socket.on('message', text => {
 });
 
 document.querySelector('button').onclick = () => {
-
     const text = document.querySelector('input').value;
     socket.emit('message', text)
-    
+}
+
+document.querySelector('button').onkeypress = () => {
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
 }
 
 // Regular Websockets
