778abb3679c3012a6719429b31fef94fcde5e40c
bonjin-all <bonjin.app@gmail.com>
2021-06-17 14:27:09 +0900

    Update express: static 경로 추가. main.html 추가

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use('/public', express.static('public'));
+
 app.listen(3000, () => {
     console.log('start! express server on port 3000');
+});
+
+app.get('/', function(req, res){
+   res.send(`<h1>hello world</h1>`); 
 });
