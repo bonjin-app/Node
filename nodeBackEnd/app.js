71119b18924f800e8fbe0a3b450490e354a49539
bonjin-all <bonjin.app@gmail.com>
2021-06-17 15:01:55 +0900

    Update app.js: static 설정.

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use('/public', express.static('public'));
+// app.use('/public', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log('start! express server on port 3000');
