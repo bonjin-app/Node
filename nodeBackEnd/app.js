4b24d6a6d4e329abc8aec6f87823af565f4e69a9
bonjin-all <bonjin.app@gmail.com>
2021-06-17 16:29:19 +0900

    Update app.js: bodyParser 추가.

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
-
-// app.use('/public', express.static('public'));
-app.use(express.static('public'));
+const bodyParser = require('body-parser');
 
 app.listen(3000, () => {
     console.log('start! express server on port 3000');
 });
 
-app.get('/', function(req, res){
+// app.use('/public', express.static('public'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
 //    res.send(`<h1>hello world</h1>`); 
     res.sendFile(__dirname+'/public/main.html');
 });
 
-app.get('/main', function(req, res){
+app.get('/main', function(req, res) {
 //    res.send(`<h1>hello world</h1>`); 
     res.sendFile(__dirname+'/public/main.html');
 });
+
+app.post('/email_post', function(req, res) {
+    console.log(req.body);
+    res.send("post response");
+})
