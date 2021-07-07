33a066d48e08b0a5e7f0a8e78e145f8aa5ec371c
bonjin-app <bonjin.app@gmail.com>
2021-07-07 18:21:30 +0900

    Update Products

--- sequelize/controllers/admin/admin.ctrl.js
+++ sequelize/controllers/admin/admin.ctrl.js
@@ -30,3 +30,35 @@ exports.getProductsDetail = (req, res) => {
             res.render('admin/detail.html', { product });
         });
 }
+
+exports.getProductsEdit = (req, res) => {
+    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
+    models.product.findByPk(req.params.id).then((product) => {
+        res.render('admin/write.html', { product: product });
+    });
+};
+
+exports.postProductsEdit = (req, res) => {
+    models.product.update(
+        {
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description
+        },
+        {
+            where: { id: req.params.id }
+        }
+    ).then(() => {
+        res.redirect('/admin/products/detail/' + req.params.id);
+    });
+}
+
+exports.getProductsDelete = (req, res) => {
+    models.product.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        res.redirect('/admin/products');
+    });
+};
--- sequelize/controllers/admin/index.js
+++ sequelize/controllers/admin/index.js
@@ -5,10 +5,14 @@ const ctrl = require('./admin.ctrl');
 router.get('/products', ctrl.getProducts);
 
 router.get('/products/write', ctrl.getProductsWrite);
-
 router.post('/products/write', ctrl.postProductsWrite);
 
 router.get('/products/detail/:id', ctrl.getProductsDetail);
+
+router.get('/products/edit/:id', ctrl.getProductsEdit);
+router.post('/products/edit/:id', ctrl.postProductsEdit);
+
+router.get('/products/delete/:id', ctrl.getProductsDelete);
 
 module.exports = router;
 
