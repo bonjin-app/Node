261d9609d5e7ac1e6456737ea5aec3d438c0d154
bonjin-app <bonjin.app@gmail.com>
2021-07-07 13:58:40 +0900

    Update Database

--- sequelize/README.md
+++ sequelize/README.md
@@ -2,3 +2,9 @@
 
 # dotenv 설정
 환경설정 변수들을 dotenv 설정한다.
+
+# sequelize
+ORM
+
+# mysql2
+promiss
--- sequelize/app.js
+++ sequelize/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
-
+// db 관련
+const db = require('./models');
 
 class App {
 
-    constructor () {
+    constructor() {
         this.app = express();
-        
+
+        // db 접속
+        this.dbConnection();
+
         // 뷰엔진 셋팅
         this.setViewEngine();
 
@@ -29,63 +32,65 @@ class App {
 
         // 에러처리
         this.errorHandler();
-
-
     }
 
+    dbConnection() {
+        // DB authentication
+        db.sequelize.authenticate()
+            .then(() => {
+                console.log('Connection has been established successfully.');
+            })
+            .then(() => {
+                console.log('DB Sync complete.');
+            })
+            .catch(err => {
+                console.error('Unable to connect to the database:', err);
+            });
+    }
 
-    setMiddleWare (){
-        
+    setMiddleWare() {
+
         // 미들웨어 셋팅
         this.app.use(logger('dev'));
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
 
     }
 
-    setViewEngine (){
-
+    setViewEngine() {
         nunjucks.configure('template', {
             autoescape: true,
             express: this.app
         });
-
     }
 
-
-    setStatic (){
+    setStatic() {
         this.app.use('/uploads', express.static('uploads'));
     }
 
-    setLocals(){
-
+    setLocals() {
         // 템플릿 변수
-        this.app.use( (req, res, next) => {
+        this.app.use((req, res, next) => {
             this.app.locals.isLogin = true;
             this.app.locals.req_path = req.path;
             next();
         });
-
     }
 
-    getRouting (){
+    getRouting() {
         this.app.use(require('./controllers'))
     }
 
-    status404() {        
-        this.app.use( ( req , res, _ ) => {
+    status404() {
+        this.app.use((req, res, _) => {
             res.status(404).render('common/404.html')
         });
     }
 
     errorHandler() {
-
-        this.app.use( (err, req, res,  _ ) => {
+        this.app.use((err, req, res, _) => {
             res.status(500).render('common/500.html')
         });
-    
     }
-
 }
-
 module.exports = new App().app;
